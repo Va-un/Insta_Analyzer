105bd3529fd16df20a286c708263ac450dc1ba0c
Varun Chindage <varunchindage@gmail.com>
2024-02-26 00:22:02 +0530

    Language translator Beta Version Ready

--- Functions.py
+++ Functions.py
@@ -1,9 +1,16 @@
 import pandas as pd
-
+from googletrans import Translator
 import os
 import json
+from tqdm import tqdm
+#pip install googletrans==4.0.0-rc1
 
-def Json_csv(input_dir, output_dir):
+translator = Translator()
+
+
+
+
+def Share_Json_csv(input_dir, output_dir):
     input_dir_list = os.listdir(input_dir)
     merged_df = pd.DataFrame()  # Initialize an empty DataFrame to store merged data
 
@@ -14,29 +21,66 @@ def Json_csv(input_dir, output_dir):
         json_file_path = file
 
         if os.path.exists(json_file_path):
-            with open(json_file_path, 'r') as file:
+            with open(json_file_path, 'r',) as file:
                 json_data = json.load(file)
                 message_data = json_data['messages']
                 df = pd.json_normalize(message_data)
                 df['timestamp_ms'] = pd.to_datetime(df['timestamp_ms'], unit='ms')
                 df['timestamp_year'] = df['timestamp_ms'].dt.strftime('%Y-%m-%d')
                 df['timestamp_time'] = df['timestamp_ms'].dt.strftime('%H:%M:%S')
+
+
                 df.drop(columns=['timestamp_ms', 'is_geoblocked_for_viewer'], inplace=True)
 
 
 
                 # Merge current DataFrame with the merged DataFrame
-                merged_df = pd.concat([merged_df, df], ignore_index=True)
+
+                merged_df = pd.concat([merged_df, df], ignore_index=True,)
         else:
             print(f'File "{json_file_path}" does not exist.')
 
-    # Write the merged DataFrame to a CSV file in the output directory
+    merged_df['reactions'] = merged_df['reactions'].apply(decode_text)
     merged_csv_path = os.path.join(output_dir, 'merged_data.csv')
-    merged_df.drop(columns=['photos', 'audio_files', 'videos', 'share.profile_share_username', 'share.profile_share_name',
-                          'call_duration', 'is_unsent', 'share.link'], inplace=True)
+    try:
+        merged_df.drop(columns=['photos', 'audio_files', 'videos', 'share.profile_share_username', 'share.profile_share_name', 'is_unsent', 'share.link'], inplace=True)
+    except:
+        print("ok")
 
     merged_df['content'] = merged_df['content'].fillna('')  # Fill NaN values with an empty string
+    
     merged_df = merged_df[~merged_df['content'].str.contains('sent an attachment')]
 
-    merged_df.to_csv(merged_csv_path, index=False)
+
+    merged_df.to_csv(merged_csv_path, index=False, encoding='utf-8')
     print(f'Merged data saved to "{merged_csv_path}".')
+
+
+
+
+def translate_to_english(text):
+    try:
+        if text is not None:
+            decoded_text = text.encode('latin1').decode('utf-8')
+            english_translation = translator.translate(decoded_text, dest='en')
+            return english_translation.text
+        else:
+            decoded_text = text.encode('latin1').decode('utf-8')
+            return decoded_text
+    except:
+        return text
+
+
+
+
+
+
+
+
+def decode_text(x):
+    if isinstance(x, str):
+        return x.encode('latin1').decode('utf-8')
+    else:
+        return x
+
+
--- Main.py
+++ Main.py
@@ -1,7 +1,6 @@
 import os
+import time
 
-import pandas as pd
-import datetime
 from Functions import *
 
 input_dir ='instagram-varun_yeager-2024-02-18-fJ87WRXU\your_instagram_activity\messages\inbox'
@@ -11,23 +10,12 @@ output_dir = 'Message'
 
 
 
-#Json_csv(input_dir,output_dir)
-#perprocessor(output_dir)
+#Json_csv(input_dir, output_dir)
+df = pd.read_csv('Message/merged_data.csv')
+
+from tqdm import tqdm
 
 
-
-
-def Text_Cleaner(Prep_input, specific_text):
-    files = os.listdir(Prep_input)
-    for file in files:
-        df = pd.read_csv(os.path.join(Prep_input, file))
-
-        # Drop rows containing specific text in 'content' column
-        df = df[~df['content'].str.contains(specific_text)]
-
-        df.to_csv(os.path.join(Prep_input, file), index=False)
-
-
-' sent an attachment'
-
+for index in tqdm(range(len(df))):
+    df.at[index, 'English_Translation'] = translate_to_english(df.at[index, 'content'])
 
